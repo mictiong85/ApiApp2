833bc244b6f95f8b635c3955fb9821e56941f0c2
mictiong85 <mictiong85@yahoo.com>
2021-04-09 21:06:48 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/ApiApplication.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.thion.maikeru.apiapp2
+
+import android.app.Application
+import io.realm.Realm
+
+class ApiApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
---
+++ app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/ApiResponse.kt
@@ -0,0 +1,35 @@
+package jp.techacademy.thion.maikeru.apiapp2
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+
+data class ApiResponse(
+    @SerializedName("results")
+    val results: Results
+)
+
+data class Results(
+    @SerializedName("shop")
+    val shop: List<Shop>
+)
+
+data class Shop(
+    @SerializedName("coupon_urls")
+    val couponUrls: CouponUrls,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("logo_image")
+    val logoImage: String,
+    @SerializedName("name")
+    val name: String
+)
+
+
+data class CouponUrls(
+    @SerializedName("pc")
+    val pc: String,
+    @SerializedName("sp")
+    val sp: String
+):Serializable
+
