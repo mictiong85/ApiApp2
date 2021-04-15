0b14fdee91827176c50a3a5448d09f896d46ca15
Michael Tiong Hoe Hee <68238173+mictiong85@users.noreply.github.com>
2021-04-15 20:43:04 +0900

    Commit

--- app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/FragmentCallback.kt
+++ app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/FragmentCallback.kt
@@ -1,6 +1,9 @@
 package jp.techacademy.thion.maikeru.apiapp2
 
+
 interface FragmentCallback {
+    // Itemを押したときの処理
+    fun onClickItem(url: String)
     // お気に入り追加時の処理
     fun onAddFavorite(shop: Shop)
     // お気に入り削除時の処理
--- app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/thion/maikeru/apiapp2/WebViewActivity.kt
@@ -1,11 +1,29 @@
 package jp.techacademy.thion.maikeru.apiapp2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_web_view.*
 
-class WebViewActivity : AppCompatActivity() {
+class WebViewActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        webView.loadUrl(intent.getStringExtra(KEY_URL).toString())
     }
+
+    companion object {
+        private const val KEY_URL = "key_url"
+        fun start(activity: Activity, url: String) {
+            activity.startActivity(
+                Intent(activity, WebViewActivity::class.java).putExtra(
+                    KEY_URL,
+                    url
+                )
+            )
+        }
+    }
+
 }
